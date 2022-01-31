1ff6e4a6f3b9de79971619e2f0957169303e6236
JoeyG92 <97414871+JoeyG92@users.noreply.github.com>
2022-01-31 13:53:07 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Script");
+let client;
+client="Samantha";
+let age=99;
+const URL="http://127.0.0.1/iot";
+age=55;
+
+console.log(age);
+console.log(URL);
+client="Maria";
+
+client="Kevin";
+
+console.log("client");
+document.write(`Please, ${client} visit the site: ${URL}`);
